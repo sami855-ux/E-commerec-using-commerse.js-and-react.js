e61d5a32aac63724c941ff91910ab319dffb08c0
sami855-ux <samitale56@outlook.com>
2025-01-02 21:04:46 +0500

    Frist commit

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,11 @@
+import { Grid2 } from "@mui/material"
+
+const Products = () => {
+  return (
+    <main>
+      <Grid2 container justify="center" spacing={4}></Grid2>
+    </main>
+  )
+}
+
+export default Products
