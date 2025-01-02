cfc02fa353884e43ee6ebc24aab5369b628f7d53
sami855-ux <samitale56@outlook.com>
2025-01-02 21:17:01 +0500

    dummy product is added instead of commerce.js api for now

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,9 +1,31 @@
 import { Grid2 } from "@mui/material"
+import Product from "./Product/Product"
+
+const product = [
+  {
+    id: 1,
+    name: "Shoes",
+    description: "Running shoes",
+  },
+  {
+    id: 2,
+    name: "mackbook",
+    description: "Apple macbook",
+  },
+]
 
 const Products = () => {
   return (
     <main>
-      <Grid2 container justify="center" spacing={4}></Grid2>
+      <Grid2 container justify="center" spacing={4}>
+        {product.map((pro) => {
+          return (
+            <Grid2 item key={pro.id} xs={12} sm={6} md={4} lg={3}>
+              <Product product={pro} />
+            </Grid2>
+          )
+        })}
+      </Grid2>
     </main>
   )
 }
