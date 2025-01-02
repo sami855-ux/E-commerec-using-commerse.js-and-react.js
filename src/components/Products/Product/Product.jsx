3b6b16f595931b5ef375a583784d16eca5551199
sami855-ux <samitale56@outlook.com>
2025-01-02 21:16:03 +0500

    New Componente is added that represents each product

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,16 @@
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+} from "@mui/material"
+
+import { AddShoppingCart } from "@mui/icons-material"
+
+const Product = () => {
+  return <div></div>
+}
+
+export default Product
